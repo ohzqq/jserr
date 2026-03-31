acd65dcfaf9c5b5092995ae6a8d6e4c61eec6aff
ohzqq <iamchurkey@gmail.com>
2026-03-31 12:21:53 -0400

    ok

--- err.go
+++ err.go
@@ -1,24 +1,21 @@
 package jserr
 
 import (
-	"errors"
 	"runtime"
 	"syscall/js"
 
 	"github.com/tinywasm/fmt"
 )
 
-var NotEnoughArgsErr = errors.New("not enough args")
-
 type Error struct {
 	js.Value
 }
 
-func New(val string) *Error {
-	return &Error{Value: js.Global().Get("Error").New(val)}
+func New(val ...string) *Error {
+	return &Error{Value: js.Global().Get("Error").New(val...)}
 }
 
-func WrapError(err error) *Error {
+func Wrap(err error) *Error {
 	return New(err.Error())
 }
 
@@ -26,25 +23,15 @@ func (e *Error) Log() {
 	js.Global().Get("console").Call("log", e.Value)
 }
 
-func Log(vals ...js.Value) {
-	js.Global().Get("console").Call("log", vals...)
-}
-
 func (e *Error) Error() string {
 	return e.Call("toString").String()
-}
-
-func CheckArgs(args []js.Value) error {
-	if len(args) < 1 {
-		return NotEnoughArgsErr
-	}
-	return nil
 }
 
 func Recover() {
 	if r := recover(); r != nil {
 		pc, f, l, _ := runtime.Caller(3)
-		fmt.Printf(errMsg, f, l, runtime.FuncForPC(pc).Name(), r)
+		err := New(fmt.Sprintf(errMsg, f, l, runtime.FuncForPC(pc).Name(), r))
+		err.Log()
 	}
 }
 
@@ -54,3 +41,7 @@ file: %v
 	func: %s
 	err: %w
 `
+
+func Log(vals ...js.Value) {
+	js.Global().Get("console").Call("log", vals...)
+}
